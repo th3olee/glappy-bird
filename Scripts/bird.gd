4a4e144f845532325b84064ef85e4db879f3982b
th3olee <theolejeune995@gmail.com>
2025-12-19 14:51:34 +0100

    feat(pipes): add bird collision detection

--- Scripts/bird.gd
+++ Scripts/bird.gd
@@ -1,5 +1,10 @@
 extends RigidBody2D
 
+@onready var collision: CollisionShape2D = $CollisionShape2D
+
+signal died
+signal scored
+
 func jump() -> void:
 	print_debug("Jump")
 	apply_central_impulse(Vector2(0, -600))
@@ -8,3 +13,17 @@ func jump() -> void:
 func _input(event):
 	if event.is_action_pressed("jump"):
 		jump()
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+
+func _on_body_entered(body: Node):
+	print_debug("touch")
+	if body.is_in_group("pipes"):
+		emit_signal("died")
+		print_debug("Game Over!")
+
+	if body.is_in_group("score"):
+		emit_signal("scored")
+		print_debug("Score +1")
