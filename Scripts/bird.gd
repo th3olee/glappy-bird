9ddd3891e51a69e4284a36335293848b69849d0e
th3olee <theolejeune995@gmail.com>
2025-12-30 12:15:32 +0100

    feat(bird): add FAIL and SCORE detection

--- Scripts/bird.gd
+++ Scripts/bird.gd
@@ -1,9 +1,22 @@
 extends RigidBody2D
 
 @onready var collision: CollisionShape2D = $CollisionShape2D
-
+@onready var area: Area2D = $Area2D
 signal died
 signal scored
+
+
+func _on_body_entered(body: Node):
+	print_debug("touch")
+	if body.is_in_group("pipes"):
+		emit_signal("died")
+		print_debug("Game Over!")
+
+func _on_area_entered(area: Area2D):
+	print_debug("Area touch")
+	if area.is_in_group("score"):
+		emit_signal("scored")
+		print_debug("Score +1")
 
 func jump() -> void:
 	print_debug("Jump")
@@ -16,14 +29,4 @@ func _input(event):
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
-
-
-func _on_body_entered(body: Node):
-	print_debug("touch")
-	if body.is_in_group("pipes"):
-		emit_signal("died")
-		print_debug("Game Over!")
-
-	if body.is_in_group("score"):
-		emit_signal("scored")
-		print_debug("Score +1")
+	area.area_entered.connect(_on_area_entered)
